fe5bff796d3a7849b36d95a73a16f596af760af9
Peter Ries <nebulashooter@tutanota.com>
2023-12-17 19:29:35 -0700

    Starting the php dev

---
+++ copytoWWW.sh
@@ -0,0 +1,4 @@
+
+sudo cp /home/peter/git/codenotes/src/* /var/www/html/codenotes
+sudo cp /home/peter/git/codenotes/src/js/* /var/www/html/codenotes/js
+sudo cp /home/peter/git/codenotes/src/css/* /var/www/html/codenotes/css
